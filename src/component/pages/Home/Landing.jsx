91e345ae81da810fe9ef7cb13185eebba731bffb
sammythedeveloper <justsammy484@gmail.com>
2025-03-28 22:11:07 -0400

    landing update

--- src/component/pages/Home/Landing.jsx
+++ src/component/pages/Home/Landing.jsx
@@ -2,6 +2,7 @@ import { Hexagon } from "./Hexagon";
 import { Circle } from "./Circle";
 import torusKnot from "../../asset/torus-knot.png";
 import cube from "../../asset/cube.png";
+import { CutCornerButton } from "./CutCornerButton";
 
 export const loginItems = [
   {
@@ -18,7 +19,6 @@ export const loginItems = [
 
 export const Landing = () => {
   return (
-    <>
       <section className=" py-24 md:py-52 overflow-x-clip ">
         <div className=" container">
           <p className=" uppercase font-extrabold text-center text-zinc-500 tracking-wider">
@@ -30,7 +30,10 @@ export const Landing = () => {
           <p className=" text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto ">
             Flowture is your smart planner that helps you organize,prioritize
             and achieve moree with less effort day by day.
-          </p>
+        </p>
+        <div className=" flex justify-center mt-10">
+          <CutCornerButton>Get Started</CutCornerButton>
+        </div>
           <div className=" flex justify-center mt-24">
             <div className=" inline-flex relative z-0 ">
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -61,10 +64,17 @@ export const Landing = () => {
                 </Circle>
               </div>
             </div>
+        </div>
+        <div className=" flex justify-center flex-col items-center mt-40 md:mt-80 gap-4 ">
+          <div className=" h-10 w-5 outline outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
+            <div className=" justify-center pt-2 rounded-full "></div>
           </div>
+          <p className="uppercase text-zinc-500 font-extrabold tracking-wider">
+            Scroll to learn more
+          </p>
+        </div>
         </div>
       </section>
-    </>
   );
 };
 
